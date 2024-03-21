5fd561612403c4c6928431376378f563081aca66
Isaac Orraiz <isaacorraiz@gmail.com>
2024-03-20 23:55:48 -0400

    Working in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,17 @@
 "use client";
+import Image from "next/image";
+import logo from "@/assets/images/logo.png";
+import Link from "next/link";
 import { useState } from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
 import { LuSearch } from "react-icons/lu";
 import { GiHamburgerMenu } from "react-icons/gi";
-import Image from "next/image";
-import logo from "@/assets/images/logo.png";
-import Link from "next/link";
+import { FaUserCircle, FaSignOutAlt } from "react-icons/fa";
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   const handleMobileMenuButton = () => {
     setIsNavOpen(!isNavOpen);
@@ -25,19 +27,44 @@ const Header = () => {
         </div>
 
         <div className="flex flex-col gap-y-4 md:flex-row md:gap-x-10 md:gap-y-0">
-          <div className="flex justify-center items-center gap-x-2 lg:justify-normal">
-            <FaMapMarkerAlt className="text-2xl text-accent" />
-            <div className="text-secondary md:hidden lg:block">
+          <a
+            href="https://maps.app.goo.gl/Q6z8GDC3yW26ucEWA"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-accent flex items-center group gap-x-2"
+          >
+            <FaMapMarkerAlt className="text-2xl group-hover:text-accent-secondary" />
+            <div className="text-secondary group-hover:text-accent-secondary md:hidden lg:block">
               Turmero, Aragua.
             </div>
+          </a>
+          <div className="flex justify-center items-center gap-x-2 lg:justify-normal">
+            <a
+              href="tel:+4122994614"
+              className="text-accent flex items-center group gap-x-2"
+            >
+              <FaPhoneAlt className="text-2xl group-hover:text-accent-secondary" />
+              <div className="text-secondary group-hover:text-accent-secondary md:hidden lg:block">
+                (412) 299 4614
+              </div>
+            </a>
           </div>
 
-          <div className="flex justify-center items-center gap-x-2 lg:justify-normal">
-            <FaPhoneAlt className="text-2xl text-accent" />
-            <div className="text-secondary md:hidden lg:block">
-              (412) 299 4614
-            </div>
-          </div>
+          {isLoggedIn ? (
+            <button className="flex justify-center items-center gap-x-2 lg:justify-normal lg:hidden">
+              <FaUserCircle className="text-2xl text-accent hover:text-accent-secondary" />
+              <span className="text-md text-secondary hover:text-accent-secondary md:hidden">
+                Mi Cuenta
+              </span>
+            </button>
+          ) : (
+            <button className="flex justify-center items-center gap-x-2 lg:justify-normal lg:hidden">
+              <FaUserCircle className="text-2xl text-accent hover:text-accent-secondary" />
+              <span className="text-md text-secondary hover:text-accent-secondary md:hidden">
+                Iniciar Sesión
+              </span>
+            </button>
+          )}
 
           <button className="btn btn-sm btn-outline w-[240px] lg:w-auto mx-auto lg:mx-0">
             Agendar
@@ -182,25 +209,42 @@ const Header = () => {
                   Contactanos
                 </Link>
               </li>
+              <li>
+                <form
+                  className="relative flex gap-x-[10px]"
+                  id="form-search-header"
+                >
+                  <label
+                    htmlFor="search-input"
+                    className="flex justify-center items-center group"
+                  >
+                    <LuSearch className="text-2xl text-accent" />
+                  </label>
+                  <input
+                    type="text"
+                    id="search-input"
+                    placeholder="Buscar..."
+                    className="outline-none w-[100px] focus:w-[180px] focus:border-b-2 focus:border-accent placeholder:italic placeholder:text-base transition-all duration-150"
+                  />
+                </form>
+              </li>
             </ul>
 
-            <form
-              className="relative flex gap-x-[10px]"
-              id="form-search-header"
-            >
-              <label
-                htmlFor="search-input"
-                className="flex justify-center items-center group"
-              >
-                <LuSearch className="text-2xl text-accent" />
-              </label>
-              <input
-                type="text"
-                id="search-input"
-                placeholder="Buscar..."
-                className="outline-none w-[100px] focus:w-[180px] focus:border-b-2 focus:border-accent placeholder:italic placeholder:text-base transition-all duration-150"
-              />
-            </form>
+            {isLoggedIn ? (
+              <button className="flex flex-col items-center cursor-pointer group">
+                <FaUserCircle className="text-2xl text-accent group-hover:text-accent-secondary" />
+                <span className="text-md text-secondary group-hover:text-accent-secondary">
+                  Mi Cuenta
+                </span>
+              </button>
+            ) : (
+              <button className="flex flex-col group items-center cursor-pointer hover:text-accent-secondary pt-2">
+                <FaUserCircle className="text-2xl text-accent group-hover:text-accent-secondary" />
+                <span className="text-md text-secondary group-hover:text-accent-secondary">
+                  Iniciar Sesión
+                </span>
+              </button>
+            )}
           </nav>
         </div>
       </div>
